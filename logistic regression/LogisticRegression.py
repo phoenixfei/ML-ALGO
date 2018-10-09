556d736fe79b8bde665c89c5612ad7d897221e0f
phe <369526049@qq.com>
2018-10-09 20:55:23 +0800

    update math.exp to numpy.exp

--- logistic regression/LogisticRegression.py
+++ logistic regression/LogisticRegression.py
@@ -1,9 +1,7 @@
-import math
+import numpy as np
 #定义sigmoid函数
 def sigmoid(x):
-    return 1/(1+math.exp(-x))
+    return 1/(1+np.exp(-x))
 #对sigmoid函数求导
 def derivate_sigmoid(x):
     return sigmoid(x)*(1-sigmoid(x))
-
-
