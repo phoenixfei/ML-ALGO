7f7ac79801efa61a5fd17fa0082a3743b8549f06
phe <369526049@qq.com>
2019-02-22 19:38:15 +0800

    enter

--- math function/MathFunction.py
+++
@@ -1,4 +0,0 @@
-import 
-#定义sigmoid函数
-# def sigmoid(x):
-#     return 1/(1+)
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import numpy as np
+
 print(np.e)
 
-#test github
+var = [88.11, 95.89, 91.75, 93.16, 89.71, 89.30, 91.64, 94.55, 91.67, 93.08]
+print(np.mean(var))
+
+# arr = np.arange(1, 11)
+# print(arr)
+# np.random.shuffle(arr)
+# print(arr)
+# np.random.shuffle(arr)
+# print(arr)
+
